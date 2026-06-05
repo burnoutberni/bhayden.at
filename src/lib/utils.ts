e39bdeceeb30d8bded74cc363369a7fa0787027a
Bernhard Hayden <hello@bhayden.at>
2026-06-05 13:48:14 +0200

    Add getPressEmail/getPressMailto utilities

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,13 +5,23 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-const MAIL_USER = 'ping'
 const MAIL_DOMAIN = 'bhayden.at'
 
+const CONTACT_USER = 'ping'
+const PRESS_USER = 'press'
+
 export function getContactEmail() {
-  return `${MAIL_USER}@${MAIL_DOMAIN}`
+  return `${CONTACT_USER}@${MAIL_DOMAIN}`
 }
 
 export function getContactMailto() {
   return `mailto:${getContactEmail()}`
+}
+
+export function getPressEmail() {
+  return `${PRESS_USER}@${MAIL_DOMAIN}`
+}
+
+export function getPressMailto() {
+  return `mailto:${getPressEmail()}`
 }
